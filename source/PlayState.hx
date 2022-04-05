2ae151cbdcbd729de95c1933e25e3fd498b0797b
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-05 11:54:43 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -53,6 +53,7 @@ import lime.utils.Assets;
 import openfl.display.BlendMode;
 import openfl.display.StageQuality;
 import openfl.filters.ShaderFilter;
+import ui.Mobilecontrols;
 
 #if windows
 import Discord.DiscordClient;
@@ -227,7 +228,11 @@ class PlayState extends MusicBeatState
 	private var saveNotes:Array<Float> = [];
 
 	private var executeModchart = false;
-
+	
+        #if mobileC
+	var mcontrols:Mobilecontrols; 
+	#end
+	
 	// API stuff
 	
 	public function addObject(object:FlxBasic) { add(object); }
@@ -1273,6 +1278,30 @@ class PlayState extends MusicBeatState
 		if (loadRep)
 			replayTxt.cameras = [camHUD];
 
+                #if mobileC
+			mcontrols = new Mobilecontrols();
+			switch (mcontrols.mode)
+			{
+				case VIRTUALPAD_RIGHT | VIRTUALPAD_LEFT | VIRTUALPAD_CUSTOM:
+					controls.setVirtualPad(mcontrols._virtualPad, FULL, NONE);
+				case HITBOX:
+					controls.setHitBox(mcontrols._hitbox);
+				default:
+			}
+			trackedinputs = controls.trackedinputs;
+			controls.trackedinputs = [];
+
+			var camcontrol = new FlxCamera();
+			FlxG.cameras.add(camcontrol);
+			camcontrol.bgColor.alpha = 0;
+			mcontrols.cameras = [camcontrol];
+
+			mcontrols.visible = false;
+
+			add(mcontrols);
+		#end
+		
+			
 		// if (SONG.song == 'South')
 		// FlxG.camera.alpha = 0.7;
 		// UI_camera.zoom = 1;
@@ -1640,7 +1669,11 @@ class PlayState extends MusicBeatState
 		  });
 		}
 		else
-		{
+		{   
+		#if mobileC
+		mcontrols.visible = true;
+		#end
+								
 			generateStaticArrows(0);
 			generateStaticArrows(1);
 	
